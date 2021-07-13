b42b4cd2bc5f18f2cf3ada6cc97132b7f8be96d9
parz14 <62294775+parz14@users.noreply.github.com>
2021-07-14 01:42:02 +0530

    Add files via upload

---
+++ Implementation/Inc/Grocery.h
@@ -0,0 +1,11 @@
+#indef_grocery_H
+#define_grocey_H
+
+void Add Record();
+void calculate bill();
+void Display all();
+void Display  Bill();
+void Display Quantity();
+void Edit Bill();
+void Rate  Display();
+#endif
