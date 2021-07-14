b38b169e45030ff934d427d24451bcc0c48ca55d
parz14 <62294775+parz14@users.noreply.github.com>
2021-07-14 23:34:47 +0530

    Update test_customer_billing.c

--- Implementation/Test/test_customer_billing.c
+++ Implementation/Test/test_customer_billing.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "unity.h"
+#include "../inc/myHeader.h"
+#include "../unity/unity.h"
 #include "Customer_billing.h"
 
 /* Modify these two lines according to the project */
@@ -20,18 +22,6 @@ void tearDown(){}
 /* Start of the application test */
 int main()
 {
-/* Initiate the Unity Test Framework */
-  UNITY_BEGIN();
-
-/* Run Test functions */
-  RUN_TEST(test_searchACustomer);
-  RUN_TEST(test_payBill);
-  RUN_TEST(test_printAllCustomers);
-  RUN_TEST(test_addBalance);
-
-  /* Close the Unity Test Framework */
-  return UNITY_END();
-}
 
 /* Write all the test functions */ 
 void test_searchACustomer(void) {
@@ -49,4 +39,16 @@ void test_printAllCustomers(void) {
 
 void test_addBalance(void) {
   TEST_ASSERT_EQUAL(void, addBalance());
+}
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_searchACustomer);
+  RUN_TEST(test_payBill);
+  RUN_TEST(test_printAllCustomers);
+  RUN_TEST(test_addBalance);
+
+  /* Close the Unity Test Framework */
+  return UNITY_END();
 }
